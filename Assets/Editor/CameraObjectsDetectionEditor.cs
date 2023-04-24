4f5a1a50072ebdfcac2a6204d328b7a915231b40
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2023-04-24 19:40:43 +0200

    Field of View dla lustra

---
+++ Assets/Editor/CameraObjectsDetectionEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+[CustomEditor(typeof(CameraObjectsDetection))]
+public class CameraObjectsDetectionEditor :Editor
+{
+    private void OnSceneGUI()
+    {
+        CameraObjectsDetection detection = (CameraObjectsDetection)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(detection.transform.position, Vector3.up, Vector3.forward, 360, detection.viewRadius);
+        Vector3 viewAngleA = detection.DirFromAngle(-detection.viewAngle / 2, false);
+        Vector3 viewAngleB = detection.DirFromAngle(detection.viewAngle / 2, false);
+
+        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleA * detection.viewRadius);
+        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleB * detection.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in detection.visibleTargets) {
+            Handles.DrawLine(detection.transform.position, visibleTarget.position);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/CameraObjectsDetection.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraObjectsDetection : MonoBehaviour
+{
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", 0.1f);
+    }
+
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+        StartCoroutine("FindTargetsWithDelay", 0.1f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2f)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
