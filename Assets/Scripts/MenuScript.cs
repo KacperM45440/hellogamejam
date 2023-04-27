3530a777976c50fd0d59fefc113baa8d97e6f5f1
Kamila <kamila.mspychala@vp.pl>
2023-04-27 13:22:13 +0200

    Menu 7
    Animacja otwierania/zamykania
    przejścia pomiędzy about i play

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,9 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject AboutUI;
+    public GameObject BeforePlayUI;
 
 
+    public void GoPlay()
+    {
+        AboutUI.SetActive(false);
+        BeforePlayUI.SetActive(true);
+    }
 
+    public void GoAbout()
+    {
+        BeforePlayUI.SetActive(false);
+        AboutUI.SetActive(true);
+    }
     public void ExitGame()
     {
         SceneManager.LoadScene(-1);
