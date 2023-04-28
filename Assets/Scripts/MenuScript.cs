aadd329f9f39baa8f200288b90be5a6614bd43b3
Kamila <kamila.mspychala@vp.pl>
2023-04-29 00:33:19 +0200

    Menu9

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,23 +9,28 @@ public class MenuScript : MonoBehaviour
 
     public GameObject AboutUI;
      public GameObject ExitUI;
+    public GameObject InsideSuitcaseUI;
+    public AudioSource SkrzypEffect;
 
     public void GoPlay()
     {
         ExitUI.SetActive(false);
         AboutUI.SetActive(false);
+        SkrzypEffect.Play();
     }   
 
     public void GoAbout()
     {
         ExitUI.SetActive(false);
         AboutUI.SetActive(true);
+        SkrzypEffect.Play();
     }
 
     public void SureExit()
     {   
         AboutUI.SetActive(false);
         ExitUI.SetActive(true);
+        SkrzypEffect.Play();
     }
     public void ExitGame()
     {
