672285991a8d54e15bc8fc47d4afd6cd4786ce86
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-11-07 02:05:38 +0100

    Menu cleanup - part 1
    Game's back in development? Who knows?

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,84 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     [SerializeField] private Animator caseAnimatorRef;
-    public GameObject AboutUI;
-    public GameObject SureExitUI;
-    public GameObject InsideGameUI;
-    public Button playButton;
-    public Button optionsButton;
-    public Button exitButton;
-    public Button backButton;
+
+    [SerializeField] private GameObject AboutUI;
+    [SerializeField] private GameObject SureExitUI;
+    [SerializeField] private GameObject InsideGameUI;
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button backButton;
 
     private void Awake()
     {
-        try
-        {
-            Destroy(PlayerReference.Instance.transform.parent.gameObject);
-            caseAnimatorRef.SetTrigger("MenuAnimClose");
-        }
-        catch
-        {
-            Debug.Log("nie ma go");
-        }
+        RemovePlayerFromGame();
     }
 
     void Update()
     {
-        if (!Cursor.visible) { Cursor.visible = true; }
+        ShowCursor();
     }
 
-    //Dzia³a git
-    public void GoPlay()
+    private void RemovePlayerFromGame()
+    {
+        if (PlayerReference.Instance != null)
+        {
+            Destroy(PlayerReference.Instance.transform.parent.gameObject);
+            caseAnimatorRef.SetTrigger("MenuAnimClose");
+        }
+        else
+        {
+            //If menu scene is the first scene loaded (game launch), ignore error
+            Debug.LogError("Menu - Player instance not found.");
+        }
+    }
+
+    private void ShowCursor()
+    {
+        Cursor.visible = true;
+    }
+
+    public void PlayButton()
     {
         playButton.interactable= false;
         optionsButton.interactable = false;
         exitButton.interactable = false;
-        InsideGameUI.SetActive(true);
-        AboutUI.SetActive(false);
+
         SureExitUI.SetActive(false);
-        //StartCoroutine(WaitStart());
+        InsideGameUI.SetActive(true);
+        AboutUI.SetActive(false);   
     }
 
-    //Dzia³a git
-    public void GoAbout()
+    public void OptionsButton()
     {
         playButton.interactable = false;
         optionsButton.interactable = false;
         exitButton.interactable = false;
+
         SureExitUI.SetActive(false);
         AboutUI.SetActive(true);
     }
 
-    public void SureExit()
+    public void ExitButton()
     {
         playButton.interactable = false;
         optionsButton.interactable = false;
         exitButton.interactable = false;
+
         SureExitUI.SetActive(true);
-        AboutUI.SetActive(false);
         InsideGameUI.SetActive(false);
+        AboutUI.SetActive(false);
     }
 
     public void BackButton()
     {
-        backButton.interactable = true;
         playButton.interactable = true;
         optionsButton.interactable = true;
         exitButton.interactable = true;
-
+        backButton.interactable = true;
     }
 
-    //tu jest git
     public void ExitGame()
     {
-        exitButton.interactable = false;
-        backButton.interactable = false;
         Application.Quit();
     }
 } 
